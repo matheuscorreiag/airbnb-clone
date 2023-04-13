05fab3b0f3011bf021211fc8f7a41bd2a7bb9881
matheus correia <matheuscorreiags@gmail.com>
2023-04-13 00:45:25 -0300

    wip: start listing created houses

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,8 @@ export default async function RootLayout({
         <RentModal />
         <RegisterModal />
         <Navbar currentUser={currentUser} />
-        {children}
+
+        <div className="pb-20 pt-28">{children}</div>
       </body>
     </html>
   );
