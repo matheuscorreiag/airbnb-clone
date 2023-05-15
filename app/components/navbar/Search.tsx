b4fe3a6aa991c34c7503a7bba041ac44b548bcbf
Matheus Correia <matheuscorreiags@gmail.com>
2023-05-15 12:15:21 -0300

    feat: search labels

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -2,10 +2,54 @@
 
 import { BiSearch } from "react-icons/bi";
 import useSearchModal from "../../hooks/useSearchModal";
+import { useSearchParams } from "next/navigation";
+import useCountries from "../../hooks/useCountries";
+import { useMemo } from "react";
+import { differenceInDays } from "date-fns";
 
 export const Search = () => {
   const searchModal = useSearchModal();
+  const params = useSearchParams();
+  const { getByValue } = useCountries();
 
+  const locationValue = params?.get("locationValue");
+  const startDate = params?.get("startDate");
+  const endDate = params?.get("endDate");
+  const guestCount = params?.get("guestCount");
+
+  const locationLabel = useMemo(() => {
+    if (locationValue) {
+      return getByValue(locationValue as string)?.label;
+    }
+
+    return "Anywhere";
+  }, [getByValue, locationValue]);
+
+  const durationLabel = useMemo(() => {
+    if (startDate && endDate) {
+      const start = new Date(startDate as string);
+      const end = new Date(endDate as string);
+      let diff = differenceInDays(end, start);
+
+      if (diff === 0) {
+        diff = 1;
+      }
+
+      return `${diff} days`;
+    }
+
+    return "Any Week";
+
+    return "Anyweek";
+  }, [startDate, endDate]);
+
+  const guestLabel = useMemo(() => {
+    if (guestCount) {
+      return `${guestCount} Guests`;
+    }
+
+    return "Add Guests";
+  }, [guestCount]);
   return (
     <div
       onClick={searchModal.onOpen}
@@ -21,7 +65,7 @@ export const Search = () => {
         cursor-pointer"
     >
       <div className="flex flex-row items-center justify-between ">
-        <div className="px-6 text-sm font-semibold ">Anywhere</div>
+        <div className="px-6 text-sm font-semibold ">{locationLabel}</div>
         <div
           className="
         hidden
@@ -34,10 +78,10 @@ export const Search = () => {
         text-center
         "
         >
-          Anyweek
+          {durationLabel}
         </div>
         <div className="flex flex-row items-center gap-3 pl-6 pr-2 text-sm text-gray-600 ">
-          <div className="hidden sm:block">Add Guests</div>
+          <div className="hidden sm:block">{guestLabel}</div>
           <div className="p-2 text-white rounded-full bg-rose-500 ">
             <BiSearch size={28} />
           </div>
