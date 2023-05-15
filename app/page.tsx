fabbb30a0e0139edeaf42ac7db0539ff341e7ca7
Matheus Correia <matheuscorreiags@gmail.com>
2023-05-15 12:20:30 -0300

    fix: activate vercel deploy

--- app/page.tsx
+++ app/page.tsx
@@ -26,8 +26,7 @@ export default async function Home({ searchParams }: HomeProps) {
       lg:grid-cols-4
       xl:grid-cols-5
       2xl:grid-cols-6
-      gap-8
-    "
+      gap-8"
       >
         {listings.map((listing) => {
           return (
